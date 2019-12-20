8c3934dc27526a89202b9c84f8da8aa0eb9ec0c8
Digital-cloud-one <yuriyrostun@gmail.com>
2019-12-20 15:24:31 +0200

    test change name

--- src/net/dark/game/Main.java
+++ src/net/dark/game/Main.java
@@ -2,6 +2,7 @@ package net.dark.game;
 
 public class Main {
     public static void main(String[] args) {
+        //test 1
 ///////////////* E n c a p s u l a t i o n *//////////////////
         MagicBall.setStringArrayList("The answer is:   Very doubtful");
         MagicBall.setStringArrayList("The answer is:   Outlook is good");
