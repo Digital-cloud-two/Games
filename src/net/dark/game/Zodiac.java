95e6589ef11b014bf55e1640ef9b0a9f0a6da1ce
Digital-cloud-one <yuriyrostun@gmail.com>
2020-01-02 14:51:03 +0200

    added method: takeAndCheckUserSolution

--- src/net/dark/game/Zodiac.java
+++ src/net/dark/game/Zodiac.java
@@ -1,6 +1,10 @@
 package net.dark.game;
 
+import java.util.Scanner;
+
 public class Zodiac extends Games {
+
+    private static Scanner scannerUserInformation = new Scanner(System.in);
 
     @Override
     public void starting() {
@@ -8,7 +12,7 @@ public class Zodiac extends Games {
     }
 
     private static void begin() {
-        showWelcomeText();
+
     }
 
     private static void showWelcomeText() {
@@ -16,7 +20,7 @@ public class Zodiac extends Games {
                 "\nYou're welcome!!!");
     }
 
-    private static void showNextTimeText() {
+    private static void showTextNextTime() {
         System.out.println("Next time!!!");
     }
 
@@ -24,7 +28,34 @@ public class Zodiac extends Games {
         System.out.println("Something is wrong!!!");
     }
 
-    private static void whatDoYouWannaKnow(){
-
+    private static void showTextWhatDoYouWannaKnow() {
+        System.out.println("Choose the number what you wanna know:" +
+                "\n                   1. FIND YOUR SIGN" +
+                "\n                   2. INFORMATION ON ASTROLOGY" +
+                "\n                   3. LOVE COMPATIBILITY" +
+                "\n                   4. Exit");
     }
+
+    private static void takeAndCheckUserSolution() {
+        int userSolutionWhatHeWants;
+        while (true) {
+            Scanner scannerUserInformation = new Scanner(System.in);
+            try {
+                userSolutionWhatHeWants = scannerUserInformation.nextInt();
+            } catch (Exception e) {                                         // What kind of mistake is better to catch?
+                System.out.println("Something is wrong!!!  Try again.");
+                continue;
+            }
+
+            if (userSolutionWhatHeWants >= 1 & userSolutionWhatHeWants <= 4) {
+
+            } else {
+                System.out.println("Chose is wrong! Try again.");
+                continue;
+            }
+            return;
+        }
+    }
+
+
 }
