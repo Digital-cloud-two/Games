f8f33f32ef2396463e7583b936f2d26dfbd4038d
Digital-cloud-one <yuriyrostun@gmail.com>
2021-03-30 09:27:56 +0300

    upDate

--- src/net/dark/game/Main.java
+++ src/net/dark/game/Main.java
@@ -3,7 +3,7 @@ package net.dark.game;
 public class Main {
     public static void main(String[] args) {
         WriteJsonToFileInJava.methodWriteJSONtoFileInJava();
-///////////////* E n c a p s u l a t i o n *//////////////////
+
         MagicBall.setStringArrayList("The answer is:   Very doubtful");
         MagicBall.setStringArrayList("The answer is:   Outlook is good");
         MagicBall.setStringArrayList("The answer is:   Outlook is not very good");
@@ -11,7 +11,6 @@ public class Main {
         MagicBall.setStringArrayList("The answer is:   Concentrate and ask again");
         MagicBall.setStringArrayList("The answer is:   Most likely");
         MagicBall.setStringArrayList("The answer is:   Do not count on it");
-        // Finish  E n c a p s u l a t i o n
 
         ChooseTheGame chooseTheGame = new ChooseTheGame();
         chooseTheGame.start();
